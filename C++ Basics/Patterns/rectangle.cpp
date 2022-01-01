17309a8e95ce4914e826a071e275565212a0efe2
soujanya hazra <soujanya0105@gmail.com>
2022-01-01 21:44:22 +0530

    rectange pattern added

---
+++ C++ Basics/Patterns/rectangle.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+// Print the pattern
+//  ****
+//  ****
+//  ****
+//  ****
+//  ****
+
+int main()
+{
+    int rows, cols;
+    cout << "Enter the number of rows : ";
+    cin >> rows;
+    cout << "Enter the number of columns : ";
+    cin >> cols;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= cols; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
